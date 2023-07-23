9a5e8cc9b353ce011fb1e482f7677f8d6833163c
MatiasBarreto93 <barretomatias93@gmail.com>
2023-07-23 20:33:37 -0300

    Stock Fix

---
+++ src/components/table/StockFull.tsx
@@ -0,0 +1,7 @@
+import {CheckCircleFill} from "react-bootstrap-icons";
+
+export const StockFull = () => {
+    return(
+        <div style={{fontWeight: "bold", color:"#0070ff"}}><CheckCircleFill color={"#0070ff"} size={20}/></div>
+    )
+}
